85d17ecc13dccd3314b845609f09660bacbec211
Shanky Munjal <shankymunjal89@gmail.com>
2016-11-05 15:32:59 +0530

    find nearby locations from postgres is done

--- server/handlers/location_handler.py
+++ server/handlers/location_handler.py
@@ -5,11 +5,16 @@ import json
 class LocationHandler(Resource):
     def get(self, location_name):
     	from server.models.location import Location, db
-    	location = db.session.query(Location).filter(Location.name == location_name)
+    	location = db.session.query(Location).filter(Location.name == location_name).first()
     	if location:
-    		result = db.engine.execute(
+    		results = db.engine.execute(
     			"select * from locations WHERE earth_box(ll_to_earth({latitude}, {longitude}), 10) @> ll_to_earth(locations.latitude, locations.longitude)".format(latitude=location.latitude, longitude=location.longitude)
-    		)
+    			)
+    		nearby_locations = []
+    		for row in results:
+			
+			    nearby_locations.append({'name': row.name, 'latitude': row.latitude, 'longitude': row.longitude})
+    		return {"nearby_locations": nearby_locations}
     	else:
     		return {"error": "Location not found"}
 
